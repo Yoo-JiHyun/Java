cd0334a6fb635a868773a95bc883bcd058bdfd1d
Yoo-JiHyun <luhyun26@gmail.com>
2024-08-09 17:19:11 +0900

    실습코드

---
+++ Java/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test {
+	
+	int a = 10;
+	int b = 20;
+	
+	public static int sun(int x, int y) {
+		int result = x + y;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		int 
+	}
+}
