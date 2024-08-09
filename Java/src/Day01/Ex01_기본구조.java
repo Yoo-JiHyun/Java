e5ba437084bc1938ade1acefddb2a686d04d3272
Yoo-JiHyun <luhyun26@gmail.com>
2024-08-09 18:25:34 +0900

    기본구조

---
+++ Java/src/Day01/Ex01_기본구조.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class Ex01_기본구조 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java/src/test/Test.java
+++ Java/src/test/Test.java
@@ -5,13 +5,16 @@ public class Test {
 	int a = 10;
 	int b = 20;
 	
-	public static int sun(int x, int y) {
+	public static int sum(int x, int y) {
 		int result = x + y;
 		return result;
 	}
 	
 	public static void main(String[] args) {
 		Test test = new Test();
-		int 
+		int answer = sum(test.a, test.b);
+		
+		System.out.print("a + b = ");
+		System.out.println(answer);
 	}
 }
