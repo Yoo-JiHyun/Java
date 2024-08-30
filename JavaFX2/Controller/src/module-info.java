e9f3e52c43997210230781b4f6744d0daf0a33b8
Yoo-JiHyun <luhyun26@gmail.com>
2024-08-30 19:09:55 +0900

    Add files via upload

---
+++ JavaFX2/Controller/src/module-info.java
@@ -0,0 +1,7 @@
+module Controller {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
---
+++ JavaFX2/RandomLaunch/src/module-info.java
@@ -0,0 +1,6 @@
+module RandomLaunch {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
